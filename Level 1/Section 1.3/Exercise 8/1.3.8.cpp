19b344d7f48ce60d66170e1b4939b6c831f40d2f
SoKinWai <45207705+SoKinWai@users.noreply.github.com>
2020-08-01 22:22:02 -0500

    Add files via upload

---
+++ Level 1/Section 1.3/Exercise 8/1.3.8.cpp
@@ -0,0 +1,25 @@
+﻿//1.3 - Variables, Operators and Expressions​
+//Exercise 8
+//The following program uses assignment-operators. 
+//Predict what will be printed on screen (provide a code file with comments stating the output for each line). 
+//The operators + and = = have a higher priority than the assignment-operators.
+#include <stdio.h>
+
+int main()
+{
+	int x = 2;
+	int y;
+	int z;
+
+	x *= 3 + 2;
+	printf("x=%d\n", x);    /*x equals 10. x=x*(3+2) */
+
+	x *= y = z = 4;
+	printf("x=%d\n", x);   /*x equals 40. x=x*4 */
+
+	x = y == z;
+	printf("x=%d\n", x);   /* x equals 1. From previous part, y and z equals 4, so (y==z) is true. Then x equals 1. */
+	
+	return 0;
+
+}
