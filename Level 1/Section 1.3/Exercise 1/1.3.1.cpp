0f5bd5c53e747559929967b7ea0880a0f8bd062b
SoKinWai <45207705+SoKinWai@users.noreply.github.com>
2020-08-01 22:12:55 -0500

    Add files via upload

---
+++ Level 1/Section 1.3/Exercise 1/1.3.1.cpp
@@ -0,0 +1,14 @@
+﻿//1.3 - Variables, Operators and Expressions​
+//Exercise 1
+//printf() is a standard function. Each compiler will support this function. 
+//Between the () is stated what has to be printed (on screen). 
+//Create a C-program that prints the following when executed:
+
+#include <stdio.h>
+
+void main() 
+{
+	/* my first program in C */
+	printf("My first C-program is a fact! Good, isn¡¯t it ? \n");
+
+}
