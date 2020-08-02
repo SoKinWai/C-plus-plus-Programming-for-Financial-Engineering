199f39e2e172814311e828491ebc867706230f48
SoKinWai <45207705+SoKinWai@users.noreply.github.com>
2020-08-01 22:16:10 -0500

    Add files via upload

---
+++ Level 1/Section 1.3/Exercise 2/1.3.2.cpp
@@ -0,0 +1,24 @@
+﻿//1.3 - Variables, Operators and Expressions​
+//Exercise 2
+//Write a program that calculates the surface of a triangle with one 90 degree angle. 
+//The formula is half the height multiplied by the base.
+//The program should take an input from the user (base & height), and output the result.
+
+
+#include <stdio.h>
+
+void main() 
+{
+	double base;         /*Base is the base of the right triangle.*/
+	double height;       /*Height is the height of the right triangle.*/
+ 	double surface;           /*Surface is the area of the right triangle.*/
+    
+	printf("Input base and height of the right triangle:\n");
+	
+	scanf_s("%lf,%lf", &base, &height);  /*Please put a "," between base and height.*/
+	
+	surface = 0.5*base*height;			/*The formula is half the height multiplied by the base.*/
+
+	printf("Surface of the right triangleis: %f\n", surface);
+
+}
