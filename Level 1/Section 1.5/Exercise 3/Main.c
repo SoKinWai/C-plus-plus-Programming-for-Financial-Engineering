c56edfd44ab3745dacf5e8a496e994c1cde1ddcf
SoKinWai <45207705+SoKinWai@users.noreply.github.com>
2020-08-02 15:37:27 -0500

    Add files via upload

---
+++ Level 1/Section 1.5/Exercise 3/Main.c
@@ -0,0 +1,20 @@
+﻿//1.5 - Functions and Scope of Variables​
+//Exercise 3
+//Write a program that consists of two source-files. 
+//The first (Main.c) contains the main() function and gives the variable i a value. 
+//The second source-file (Print.c) multiplies i by 2 and prints it.
+//Print.c contains the function print() which can be called from main().
+
+#include<stdio.h>
+
+void main()
+{
+	double i;
+	
+	printf("Please enter a number for i:\n");
+	
+	scanf_s("%lf", &i);
+	
+	print(i);
+	
+}
---
+++ Level 1/Section 1.5/Exercise 3/Print.c
@@ -0,0 +1,18 @@
+﻿//1.5 - Functions and Scope of Variables​
+//Exercise 3
+//Write a program that consists of two source-files. 
+//The first (Main.c) contains the main() function and gives the variable i a value. 
+//The second source-file (Print.c) multiplies i by 2 and prints it.
+//Print.c contains the function print() which can be called from main().
+
+#include<stdio.h>
+
+double print(double i)
+{
+	
+	double j;
+	j = i*2;
+	printf("The value of j is:%lf\n", j);
+
+	return j;
+}
