f877518ae08d97546d414058729ca9ba93d3bc5c
SoKinWai <45207705+SoKinWai@users.noreply.github.com>
2020-08-01 22:18:58 -0500

    Add files via upload

---
+++ Level 1/Section 1.3/Exercise 5/1.3.5.cpp
@@ -0,0 +1,24 @@
+﻿//1.3 - Variables, Operators and Expressions​
+//Exercise 5
+//Create a C-program that clearly shows the difference between --i and i--.
+
+# include <stdio.h>
+
+int main()
+{
+	int i=10;         
+	int j = 10;
+	int a;
+	int b;
+	
+	a = i--;
+	
+	printf("value of a:%d",a);      /*Equivalent code: a=i; a equals 10.*/
+	printf("\nvalue of i:%d", i);     /*Equivalent code:i=i-1;i equals 9.*/
+
+	b = --j;
+	printf("\nvalue of b:%d", b);      /*Equivalent code:b=j-1;b equals 9.*/
+	printf("\nvalue of j:%d", j);       /*Equivalent code:j=j-1;j equals 9.*/
+	
+	return 0;
+}
