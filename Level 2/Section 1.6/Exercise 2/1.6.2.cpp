0d871fedcde40b7f2a7e1f5a1627d98a385b85d7
SoKinWai <45207705+SoKinWai@users.noreply.github.com>
2020-08-03 13:31:53 -0500

    Add files via upload

---
+++ Level 2/Section 1.6/Exercise 2/1.6.2.cpp
@@ -0,0 +1,23 @@
+﻿//Level 2
+//1.6 - The Preprocessor​
+//Exercise 2
+//Use two macros to get the maximum.
+//HW 1.6.2
+//1.6.2.cpp
+
+
+#include<stdio.h>
+#include"Defs.h"
+
+
+int main()
+{
+	int x;
+	int y;
+	int z;
+	printf("Please enter three integers x,y and z(please add , between x,y and z):\n");     //Input 3 integers x,y and z. Don't forget add , bewteen x,y and z
+    scanf_s("%d,%d,%d", &x, &y, &z);
+	printf("The maximum value of x and y is:%d\n", MAX2(x, y));		//Print the max value of x and y by using MAX2(x,y).
+	printf("The maximum value of x,y and z is:%d\n",MAX3(x, y, z));		//Print the max value of x,y and z by using MAX3(x,y).
+	return 0;
+}
---
+++ Level 2/Section 1.6/Exercise 2/Defs.h
@@ -0,0 +1,18 @@
+﻿//Level 2
+//1.6 - The Preprocessor​
+//Exercise 2
+//Two macros MAX2(x,y) and MAX3(x,y,z)
+//Create the two macros MAX2(x,y) and MAX3(x,y,z).
+//These macros must return the maximum value of the given arguments. 
+//Let the macro MAX3 make use of the macro MAX2.
+//Add these macros to the file “Defs.h”.
+
+
+
+#ifndef MAX2
+#define MAX2(x, y)	((x > y) ? x : y)
+#endif
+
+#ifndef MAX3
+#define MAX3(x, y, z)	(MAX2(MAX2(x, y), z))
+#endif
