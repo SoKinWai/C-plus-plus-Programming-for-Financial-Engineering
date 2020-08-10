b341f25798578e24930b7b3c43622f023e3cc4d1
SoKinWai <45207705+SoKinWai@users.noreply.github.com>
2020-08-10 01:34:36 -0500

    Add files via upload

---
+++ Level 3/Section 2.3/Exercise 4/Point.hpp
@@ -0,0 +1,47 @@
+﻿//Level 3
+//2.3 - Improving your Classes​
+//Exercise 4: Const Functions​
+//Mark the x-coordinate getter as const by making it a const function. 
+//Do this also for the y-coordinate getter and the Distance() and ToString() functions.
+
+#ifndef Point_HPP
+#define Point_HPP
+#include <iostream>
+using namespace std;
+
+class Point
+{
+private:
+	//x- and y-coordinates 
+	double m_x;
+	double m_y;
+
+public:
+	Point();								//Default constructor.
+	Point(const Point& p);					//Copy constructor.
+	Point(double xval, double yval);		//The constructor that accepts x- and y-coordinates.
+
+
+	~Point();							    //Destructor
+
+	//Modifiers
+	//Setter functions for x- and y-coordinates.
+	void X(double new_x);
+	void Y(double new_y);
+
+	//To access functions
+	//Getter functions for x- and y-coordinates.
+	double X() const;
+	double Y() const;
+
+	string ToString() const;						//To string description of the point.
+
+	//The distance between the origin and the point 1.
+	double Distance() const;
+
+	//To calculate the distance between two points.
+	//Pass the Distance()  function as “const reference”
+	double Distance(const Point& p) const;
+};
+
+#endif
