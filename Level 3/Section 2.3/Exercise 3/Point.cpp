eb771c01e7fff0f57c5b5ac480fb5e1535343b9b
SoKinWai <45207705+SoKinWai@users.noreply.github.com>
2020-08-10 01:16:01 -0500

    Add files via upload

---
+++ Level 3/Section 2.3/Exercise 3/Point.cpp
@@ -0,0 +1,71 @@
+﻿//Level 3
+//2.3 - Improving your Classes​
+//Exercise 3: Function Overloading​
+//Function Overloading​.
+//Rename the DistanceOrigin() function to Distance().
+//Rename the SetX() and GetX() functions to just X(). 
+//The same is true for the setter and getter of the y-coordinate.
+
+#include "Point.hpp"
+#include <cmath>
+#include <sstream>
+#include <iostream>
+using namespace std;
+
+Point::Point()														//Default constructor
+{
+	cout << "This is the default constructor." << endl;
+}
+
+Point::Point(const Point& p) :m_x(p.m_x), m_y(p.m_y)				//Copy constructor
+{
+	cout << "This is the copy constructor." << endl;
+}
+
+Point::Point(double xval, double yval) : m_x(xval), m_y(yval)		//The constructor that accepts x- and y-coordinates.
+{
+	cout << "This is the constructor that accepts x- and y-coordinates." << endl;
+}
+
+Point::~Point()														//Destructor.
+{
+	cout << "This is the destructor." << endl;
+}
+
+void Point::X(double new_x)
+{
+	m_x = new_x;
+}
+
+void Point::Y(double new_y)
+{
+	m_y = new_y;
+}
+
+double Point::X()
+{
+	return m_x;
+}
+
+double Point::Y()
+{
+	return m_y;
+}
+
+string Point::ToString()
+{
+	stringstream ss;
+	ss << "Point(" << m_x << "," << m_y << ")";
+	return ss.str();
+}
+
+double Point::Distance()
+{
+	return sqrt(pow(m_x, 2) + pow(m_y, 2));
+}
+
+
+double Point::Distance(const Point& p)
+{
+	return sqrt(pow(m_x - p.m_x, 2) + pow(m_y - p.m_y, 2));
+}
---
+++ Level 3/Section 2.3/Exercise 3/Point.hpp
@@ -0,0 +1,50 @@
+﻿//Level 3
+//2.3 - Improving your Classes​
+//Exercise 3: Function Overloading​
+//Function Overloading​.
+//Rename the DistanceOrigin() function to Distance().
+//Rename the SetX() and GetX() functions to just X(). 
+//The same is true for the setter and getter of the y-coordinate.
+
+#ifndef Point_HPP
+#define Point_HPP
+#include <iostream>
+using namespace std;
+
+class Point
+{
+private:
+	//x- and y-coordinates 
+	double m_x;
+	double m_y;
+
+public:
+	Point();								//Default constructor.
+	Point(const Point& p);					//Copy constructor.
+	Point(double xval, double yval);		//The constructor that accepts x- and y-coordinates.
+
+
+	~Point();							    //Destructor
+	
+	//Modifiers
+	//Setter functions for x- and y-coordinates.
+	void X(double new_x);
+	void Y(double new_y);
+
+	//Selectors
+	//To access functions
+	//Getter functions for x- and y-coordinates.
+	double X();
+	double Y();
+
+	string ToString();						//To string description of the point.
+
+	//The distance between the origin and the point 1.
+	double Distance();
+	
+	//To calculate the distance between two points.
+	//Pass the Distance()  function as “const reference”
+	double Distance(const Point& p);
+};
+
+#endif
