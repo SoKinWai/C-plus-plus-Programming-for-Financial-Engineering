23a4594d03972453220defb62bc6248e0156277e
SoKinWai <45207705+SoKinWai@users.noreply.github.com>
2020-08-10 02:19:29 -0500

    Add files via upload

---
+++ Level 3/Section 2.3/Exercise 5/Line.cpp
@@ -0,0 +1,62 @@
+﻿//Level 3
+//2.3 - Improving your Classes​
+//Exercise 5: Line Class​
+//Create a Line class.
+//The Line class has a start- and an end-point.
+//The Line class should have two Point objects as data members. 
+//Use const arguments, const functions and pass objects by reference where applicable.
+
+#include "Line.hpp"
+#include "Point.hpp"
+#include <cmath>
+#include <sstream>
+#include <iostream>
+using namespace std;
+
+Line::Line():p_start(0,0),p_end(0,0)
+{
+}
+
+Line::Line(const Point& p_start, const Point& p_end) : p_start(p_start), p_end(p_end)
+{
+}
+
+Line::Line(const Line& l) : p_start(l.p_start), p_end(l.p_end)
+{
+}
+
+Line::~Line()
+{
+}
+
+void Line::P1(const Point& p_newstart)
+{
+	p_start = p_newstart;
+}
+
+void Line::P2(const Point& p_newend)
+{
+	p_end = p_newend;
+}
+
+const Point& Line::P1() const
+{
+	return p_start;
+}
+
+const Point& Line::P2() const
+{
+	return p_end;
+}
+
+string Line::ToString() const
+{
+	stringstream ss;
+	ss<< "The line of " <<p_start.ToString() << " to " << p_end.ToString();
+	return ss.str();
+}
+
+double Line::Length() const
+{
+	return p_start.Distance(p_end);
+}
---
+++ Level 3/Section 2.3/Exercise 5/Line.hpp
@@ -0,0 +1,47 @@
+﻿//Level 3
+//2.3 - Improving your Classes​
+//Exercise 5: Line Class​
+//Create a Line class.
+//The Line class has a start- and an end-point.
+//The Line class should have two Point objects as data members. 
+//Use const arguments, const functions and pass objects by reference where applicable.
+
+
+#ifndef Line_HPP
+#define Line_HPP
+#include "Point.hpp"
+#include <iostream>
+using namespace std;
+
+class Line
+{
+private:
+	Point p_start;													//The start point.
+	Point p_end;													//The end point.
+
+public:
+	Line();															//Default constructor.
+	Line(const Point& p_start, const Point& p_end);					//Constructor with a start- and end-point.
+	Line(const Line& l);										    //Copy constructor.
+	
+	~Line();														//Destructor.
+	
+	//Modifiers
+	//Overloaded setters for the start- and end-point.
+	void P1(const Point& p_newstart);
+	void P2(const Point& p_newend);
+	
+	//Selectors															
+	//To access functions.
+	//Overloaded getters for the start- and end-point.
+	const Point& P1() const;
+	const Point& P2() const;
+
+	//A ToString() function that returns a description of the line.
+	string ToString() const;
+
+	//A Length() function that returns the length of the line. 
+	double Length() const;
+};
+#endif
+
