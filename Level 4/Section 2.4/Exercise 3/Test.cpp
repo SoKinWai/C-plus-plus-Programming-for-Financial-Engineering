209067ac660aac5d205be3ed8c5135630aa5f490
SoKinWai <45207705+SoKinWai@users.noreply.github.com>
2020-08-11 22:54:37 -0500

    Add files via upload

---
+++ Level 4/Section 2.4/Exercise 3/Test.cpp
@@ -0,0 +1,22 @@
+﻿//Constructors as conversion operator​
+//To prevent the usage of constructors are implicit conversion operators, declare the constructor as explicit:
+//explicit Point(double value)
+// still use the constructor as conversion operator but then explicitly:
+//if (p == (Point)1.0) cout << "Equal!" << endl
+//Test program.
+
+#include "Point.hpp"
+#include<string>
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	Point p(1.0, 1.0);
+	
+	//if (p == 1.0) cout << "Equal!" << endl;
+	//else cout << "Not equal" << endl;
+
+	if (p == (Point)1.0) cout << "Equal!" << endl;
+	else cout << "Not equal" << endl;
+}
