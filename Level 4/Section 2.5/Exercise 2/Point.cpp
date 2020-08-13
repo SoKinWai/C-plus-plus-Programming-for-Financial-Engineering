f54b2d00ac9c5700a95002aa5b653a507176efd1
SoKinWai <45207705+SoKinWai@users.noreply.github.com>
2020-08-12 23:04:26 -0500

    Add files via upload

---
+++ Level 4/Section 2.5/Exercise 2/Point.cpp
@@ -0,0 +1,122 @@
+﻿//2.5 - The Free Store​
+//Exercise 2: Creating Array of Pointers​
+
+#include "Point.hpp"
+#include <cmath>
+#include <sstream>
+#include <iostream>
+using namespace std;
+
+Point::Point() :m_x(0), m_y(0)										//Default constructor
+{
+	cout << "This is the default constructor." << endl;
+}
+
+Point::Point(const Point& p) : m_x(p.m_x), m_y(p.m_y)				//Copy constructor
+{
+	cout << "This is the copy constructor." << endl;
+
+}
+
+Point::Point(double xval, double yval) : m_x(xval), m_y(yval)		//The constructor that accepts x- and y-coordinates.
+{
+	cout << "This is the constructor that accepts x- and y-coordinates." << endl;
+
+}
+
+Point::Point(double value) : m_x(value), m_y(value)
+{
+	cout << "declare the constructor as explicit." << endl;
+}
+
+Point::~Point()														//Destructor.
+{
+	cout << "This is the destructor." << endl;
+
+}
+
+double Point::X() const
+{
+	return m_x;
+}
+
+double Point::Y() const
+{
+	return m_y;
+}
+
+void Point::X(double new_x)
+{
+	m_x = new_x;
+}
+
+void Point::Y(double new_y)
+{
+	m_y = new_y;
+}
+
+string Point::ToString() const
+{
+	stringstream ss;
+	ss << "Point(" << m_x << "," << m_y << ")";
+	return ss.str();
+}
+
+double Point::Distance() const
+{
+	return sqrt(pow(m_x, 2) + pow(m_y, 2));
+}
+
+
+double Point::Distance(const Point& p) const
+{
+	return sqrt(pow(m_x - p.m_x, 2) + pow(m_y - p.m_y, 2));
+}
+
+Point Point::operator - () const										// Negate the coordinates.
+{
+	return Point(-m_x, -m_y);
+}
+
+Point Point::operator * (double factor) const
+{
+	return Point(m_x*factor, m_y*factor);								// Scale the coordinates.
+}
+
+Point Point::operator + (const Point& p) const
+{
+	return Point(m_x + p.m_x, m_y + p.m_y);								// Add coordinates.
+}
+
+bool Point::operator == (const Point& p) const							// Equally compare operator.
+{
+	return bool(m_x == p.m_x && m_y == p.m_y);
+
+}
+
+Point& Point::operator = (const Point& source)							// Assignment operator.
+{
+	if (this == &source)
+	{
+		return *this;
+	}
+
+	m_x = source.m_x;
+	m_y = source.m_y;
+
+	return *this;
+}
+
+Point& Point::operator *=(double factor)								// Scale the coordinates & assign.
+{
+	m_x *= factor;
+	m_y *= factor;
+
+	return *this;
+}
+
+ostream& operator << (ostream& os, const Point& p)						// Send to ostream.
+{
+	os << "Point(" << p.m_x << "," << p.m_y << ")";
+	return os;
+}
---
+++ Level 4/Section 2.5/Exercise 2/Test.cpp
@@ -0,0 +1,41 @@
+﻿//2.5 - The Free Store​
+//Exercise 2: Creating Array of Pointers​
+//With an array of Points, all points are created with the default constructor.
+//When creating an array of pointers, each element in the array must be created separately but can be created with other constructors than the default constructor.
+
+#include "Point.hpp"
+#include <string>
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	const int size = 3;
+	
+	//Create an array of Point pointers with 3 elements on the heap.
+	Point** arr_pp = new Point*[size];
+
+	//Create for each element in the array a point on the heap.
+	arr_pp[0] = new Point(2.0, 1.1);
+	arr_pp[1] = new Point(3.1, 3.2);
+	arr_pp[2] = new Point(4.2, 5.1);
+
+	//Iterate the array and print each point in the array.
+	for (int s = 0;s < 3;s++)
+	{
+		cout << *arr_pp[s] << endl;											//Use the constructors that accept x- and y-coordinates.
+	}
+
+	//Iterate the array again and delete each point in the array.
+	for (int s = 0;s < 3;s++)
+	{
+		delete arr_pp[s];
+	}
+
+	//Delete the array itself.
+	delete[] arr_pp;
+
+
+	//The name of drawing of the memory layout is So's pic.Please check that. Thanks.
+
+}
