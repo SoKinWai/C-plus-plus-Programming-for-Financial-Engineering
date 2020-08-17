bdcfff1cd86a88f81ea164aa2ea4cf951ae433c0
SoKinWai <45207705+SoKinWai@users.noreply.github.com>
2020-08-17 01:39:53 -0500

    Add files via upload

---
+++ Level 5/Section 3.5/Exercise 3/Shape.hpp
@@ -0,0 +1,32 @@
+﻿//3.5 - Polymorphism​
+//Exercise 3: Virtual Destructors
+//Print some text in the destructors of the Shape, Point and Line classes.
+
+#ifndef Shape_HPP
+#define Shape_HPP
+#include <iostream>
+using namespace std;
+
+class Shape
+{
+private:
+	//Add a data member for an id number of type int.
+	int m_id;
+
+public:
+	Shape();										//Add a default constructor
+	Shape(const Shape& s);							//Add a copy constructor that copies the id member.
+	virtual ~Shape();								//Destructor
+
+	//Add an assignment operator that copies the id member.
+	Shape& operator = (const Shape& s);				//Assignment operator
+
+	//Add a ToString() function
+	virtual string ToString() const;
+
+	//Add an ID() function
+	int ID() const;
+};
+
+#endif
+
---
+++ Level 5/Section 3.5/Exercise 3/Test.cpp
@@ -0,0 +1,34 @@
+﻿//3.5 - Polymorphism​
+//Exercise 3: Virtual Destructors
+//Print some text in the destructors of the Shape, Point and Line classes.
+//Test program.
+
+#include "Point.hpp"
+#include "Shape.hpp"
+#include "Line.hpp"
+#include <iostream>
+#include <string>
+using namespace std;
+
+void main()
+{
+	Shape* shapes[3];
+	shapes[0] = new Shape;
+	shapes[1] = new Point;
+	shapes[2] = new Line;
+
+	for (int i = 0; i != 3; i++) delete shapes[i];
+
+	//Will the proper destructors (including the destructor of the Shape base class) be called?
+	//When the Shape destructor is not as virtual, the shape destructors are called 3 times.
+	//No, they won't.
+	
+
+	//In this case, the derived class destructor will only be called when the destructor is declared virtual in the base class. 
+	//Do this in the Shape class and run the code again. Are the proper destructors called now?
+	//When the Shape destructor is as virtual, then:
+	//the shape destructors are called 5 times.
+	//the Point destructors are called 3 times.
+	//the Line destructor is called 1 time.
+	//The proper destructors are called now.
+}
